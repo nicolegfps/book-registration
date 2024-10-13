197e57a67281a7b4fed2d53af6cd88be92f7491b
nicolegfps <rissagrumal@gmail.com>
2024-10-13 08:14:41 +0800

    books table migration

--- database/migrations/2024_10_13_001153_create_books_table.php
+++ database/migrations/2024_10_13_001153_create_books_table.php
@@ -10,12 +10,16 @@ return new class extends Migration
      * Run the migrations.
      */
     public function up(): void
-    {
-        Schema::create('books', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-        });
-    }
+{
+    Schema::create('books', function (Blueprint $table) {
+        $table->id();
+        $table->string('title');
+        $table->string('author');
+        $table->string('genre');
+        $table->date('publication_date');
+        $table->timestamps();
+    });
+}
 
     /**
      * Reverse the migrations.
